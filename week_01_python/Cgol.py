34f9a53830036c55094a094f2c1e85fd9cb8a895
Emma Wingreen <emmawingreen@gmail.com>
2021-09-11 16:45:37 -0400

    added functions printBoard, setCell, and createNewBoard

--- week_01_python/Cgol.py
+++ week_01_python/Cgol.py
@@ -1,14 +1,38 @@
-# Initialize a new array of characters with default values for rows and cols
+# createNewBoard initializes a new array of characters with default values for rows and cols
+# Use a nested for loop to initialize the empty board
 def createNewBoard(rows, cols):
     matrix = []
     for i in range(rows):
       row = []
       for j in range(cols):
-        row.append(0) #initialize values to 0
+        row.append(0) #initialize values to '-'
       matrix.append(row)
-      print(row) #print out blank board
+    return matrix
+
+def setCell(board, r, c, val):
+    board[r][c] = val
+
+def printBoard(board):
+    for rows in board:
+        print(rows)
+
+
+#MAIN PROGRAM
+
+#Create a new two-dimensional array called board
+board = [[]]
 
 #Call createNewBoard function with given parameters
-createNewBoard(25, 25)
+board = createNewBoard(25, 25)
 
-# Use a nested for loop to initialize the empty board
+# Breathe life into some cells:
+setCell(board, 0, 1, 1);
+setCell(board, 1, 2, 1);
+setCell(board, 2, 0, 1);
+setCell(board, 2, 1, 1);
+setCell(board, 2, 2, 1);
+
+#Print out the initialized board
+#Should be printBoard function
+printBoard(board)
+#printBoard(board, board[rows], board[cols])
